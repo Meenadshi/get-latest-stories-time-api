e496a0627205325e12c539016e6ab80fd13acd39
Meenadshi <meenadshi20@gmail.com>
2023-09-09 00:21:10 +0530

    Add.Built the code for extracting latest 6 stories from time.com

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-//This is an empty file for now
+const express = require('express')
+const https = require('https')
+const app = express()
+const port = 3500
+
+app.get('/api/getTimeStories', async (req, res) => {
+    try{
+        const url = 'https://time.com';
+        https.get(url, (response) => {
+            let data = '';
+            response.on('data',(chunk) => {
+                data +=  chunk;
+            });
+            response.on('end', () => {
+                const latestAllStories = parseHTML(data);
+                const latestStories = latestAllStories.slice(0, 6);
+
+                res.write("[\n");
+                latestStories.forEach((story) => {
+                    res.write(JSON.stringify(story, null, 2) + '\n');
+                });
+                res.write("]");
+
+                res.end();
+            });
+        });
+    } catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
+});
+
+function parseHTML(html){
+    const latestStories = [];
+    const regex = /<li class="latest-stories__item">\s*<a href="([^"]+)">\s*<h3 class="latest-stories__item-headline">([^<]+)<\/h3>/g;
+    let match;
+    while((match = regex.exec(html)) !== null){
+            console.log("In if");
+            latestStories.push({
+                title: match[2],
+                link: match[1],
+            });
+    }
+    return latestStories;
+}
+
+app.listen(port, () => {
+    console.log("Server is running on port "+port);
+} )
